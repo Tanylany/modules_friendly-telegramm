54ba1d2cd4b8b34c2843e2296d41d68782a36e8c
Tanylany <dima.20050912@gmail.com>
2021-03-01 23:54:18 +0200

    first commit

---
+++ police.py
@@ -0,0 +1,19 @@
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.pol", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+
+    await event.edit("🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵")
+    await asyncio.sleep(0.5)
+    await event.edit("🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴")
+    await asyncio.sleep(0.5)
+    await event.edit("🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵")
+    await asyncio.sleep(0.5)
+    await event.edit("`**🚨🚨Никому ни с места! Прибыло ФСБ, всем лежать, морды в пол🚨🚨`**")
